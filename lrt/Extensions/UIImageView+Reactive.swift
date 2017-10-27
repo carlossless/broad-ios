7dbbd73cfa3ca5909e352abcb42016f027fce4c5
Karolis Stasaitis <stkarolis@gmail.com>
2017-10-27 09:09:16 +0200

    Better image management

--- lrt/Extensions/UIImageView+Reactive.swift
+++ lrt/Extensions/UIImageView+Reactive.swift
@@ -10,25 +10,34 @@ import Foundation
 import ReactiveSwift
 import Kingfisher
 
+// Make this whole thing actually reactive
+
 extension Reactive where Base: UIImageView {
     public var imageUrl: BindingTarget<URL?> {
         return makeBindingTarget { Reactive.loadImage(view: $0, url: $1) }
     }
     
-    static func loadImage(view: UIImageView, url: URL?) {
+    func imageUrl(size: CGSize) -> BindingTarget<URL?> {
+        return makeBindingTarget { Reactive.loadImage(view: $0, url: $1, size: size) }
+    }
+    
+    static func loadImage(view: UIImageView, url: URL?, size: CGSize? = nil) {
         let scale = UIScreen.main.scale
+        var options = [KingfisherOptionsInfoItem.scaleFactor(scale)]
+        if let size = size {
+            options.append(
+                KingfisherOptionsInfoItem.processor(
+                    ResizingImageProcessor(
+                        referenceSize: size,
+                        mode: ContentMode.aspectFill
+                    )
+                )
+            )
+        }
         view.kf.setImage(
             with: url,
             placeholder: nil,
-            options: [
-                KingfisherOptionsInfoItem.processor(
-                    ResizingImageProcessor(
-                        referenceSize: CGSize(width: 60, height: 40),
-                        mode: ContentMode.aspectFill
-                    )
-                ),
-                KingfisherOptionsInfoItem.scaleFactor(scale)
-            ],
+            options: options,
             progressBlock: nil,
             completionHandler: nil
         )
